e019579670538b846fbcce51cebf98b908db9a82
rafi <r.rafi1615005@gmail.com>
2023-01-21 15:14:38 +0600

    categories-added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./Home.scss"
-import ItemContainer from '../../components/ItemContainer';
 import Slider from '../../components/Slider';
+import Categories from '../../components/Categories';
 const Home = () => {
   return (
     <div className='home'>
       {/* <ItemContainer/> */}
       <Slider/>
+      <Categories/>
     </div>
   )
 }
